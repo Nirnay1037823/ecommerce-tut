e08bd43dd60f9e00ab76c1ef17e6c398b4a63de8
Nirnay1037823 <nirnay.behera@jda.com>
2024-02-07 14:52:14 +0530

    added JWT

--- src/main/java/com/example/Ecommerce/ExceptionHandler/ProductException.java
+++ src/main/java/com/example/Ecommerce/ExceptionHandler/ProductException.java
@@ -1,5 +1,20 @@
 package com.example.Ecommerce.ExceptionHandler;
 
-public class ProductException {
+public class ProductException extends Exception{
+    private static final int id = 1;
 
+    private String errorMessage;
+
+    public ProductException(String message, String errorMessage) {
+        super(message);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
